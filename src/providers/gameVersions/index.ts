f856b6c3c41ad6d1735688dd712ba7111380351e
TheKodeToad <TheKodeToad@proton.me>
2026-07-17 15:55:12 +0100

    Use fabric maven for experiments

--- src/providers/gameVersions/index.ts
+++ src/providers/gameVersions/index.ts
@@ -1,4 +1,4 @@
-import { PISTON_META } from "#common/constants/urls.ts";
+import { FABRIC_MAVEN, PISTON_META } from "#common/constants/urls.ts";
 import { HTTPCacheMode, type HTTPClient } from "#httpClient.ts";
 import { defineProvider } from "#index.ts";
 import { PistonVersion } from "#schemas/pistonMeta/pistonVersion.ts";
@@ -12,7 +12,11 @@ export default defineProvider({
 	id: "game-versions",
 
 	async provide(http): Promise<PistonVersion[]> {
-		return Promise.all([pistonMetaVersions(http)]).then((versions) =>
+		return Promise.all([
+			pistonMetaVersions(http),
+			fabricMavenVersions(http),
+
+		]).then((versions) =>
 			orderBy(
 				versions.flat(),
 				[(version) => version.releaseTime],
@@ -30,6 +34,21 @@ async function pistonMetaVersions(http: HTTPClient): Promise<PistonVersion[]> {
 			await http.getCached(
 				new URL("mc/game/version_manifest_v2.json", PISTON_META),
 				base + "/versions.json",
+			)
+		).json(),
+	);
+
+	return await getVersions(http, base, manifest.versions);
+}
+
+async function fabricMavenVersions(http: HTTPClient): Promise<PistonVersion[]> {
+	const base = "fabric-maven";
+
+	const manifest = PistonVersionManifest.parse(
+		(
+			await http.getCached(
+				new URL("net/minecraft/experimental_versions.json", FABRIC_MAVEN),
+				base + "/experimental_versions.json",
 			)
 		).json(),
 	);
